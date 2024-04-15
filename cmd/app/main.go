f1437310cf82377e3f54b5263696b8e2b8957fc3
GolZrd <egor_golyshev23@mail.ru>
2024-04-15 21:01:10 +0300

    Добавил новые пакеты и добавил обработчики

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,13 +4,19 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+
+	"gorilla_gorm/internal/controller"
 )
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", home)
-	router.HandleFunc("/notes", getNotes).Methods("GET")
-	router.HandleFunc("/note/{id:[0-9]+}", getNote).Methods("GET")
+	router.HandleFunc("/", controller.Home)
+	router.HandleFunc("/notes", controller.GetNotes)
+	router.HandleFunc("/note/{id:[0-9]+}", controller.GetNote).Methods("GET")
+
+	//router.HandleFunc("/notes/create", controller.CreateNote).Methods("POST")
+	//router.HandleFunc("/note/{id:[0-9]+}/update", controller.UpdateNote).Methods("POST")
+	//router.HandleFunc("/note/{id:[0-9]+}/delete", controller.Delete).Methods("POST")
 
 	http.ListenAndServe(":8080", router)
 }
--- internal/controller/handlers.go
+++ internal/controller/handlers.go
@@ -1,1 +1,53 @@
 package controller
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// home handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	ts, err := template.ParseFiles("web/html/homePage.html")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	w.Write([]byte("Welcome to your Home page!"))
+}
+
+func GetNotes(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Здесь будут все записи из базы данных"))
+}
+
+func GetNote(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	id := vars["id"]
+
+	w.Write([]byte("Запись с ID " + id))
+}
+
+//Создание заметки
+// func CreateNote(w http.ResponseWriter, r *http.Request) {}
+
+//Редактирование заметки
+// func UpdateNote(w http.ResponseWriter, r *http.Request) {}
+
+//Удаление заметки
+// func Delete(w http.ResponseWriter, r *http.Request) {}
