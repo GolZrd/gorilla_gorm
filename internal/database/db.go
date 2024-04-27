9a3f7c0bfbe252fbacede40bbb2307e59c0caa86
GolZrd <166915505+GolZrd@users.noreply.github.com>
2024-04-27 07:16:26 +0300

    Update db.go

--- internal/database/db.go
+++ internal/database/db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Init() *gorm.DB {
-	conn := "user=postgres password=Egolgor23 dbname=productdb sslmode=disable"
+	conn := "user=user password=Password dbname=productdb sslmode=disable"
 	db, err := gorm.Open(postgres.Open(conn), &gorm.Config{})
 	if err != nil {
 		panic(err)
